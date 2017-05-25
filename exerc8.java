ab2fb3901160a4e64f0761610e5190822a27dd46
matheusjkd <matheusjkdproducao@gmail.com>
2017-05-25 15:07:01 -0300

    Add files via upload

---
+++ exerc8.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+	public  class exerc8 {
+	public static void main (String [] args){
+		 double quad;
+		Scanner sc= new Scanner(System.in);
+		 System.out.println("digite o valor do lado do quadrado");
+        quad=sc.nextLine();
+        System.out.println("a area do quad é "(quad*quad));
+    }
+}
